5ea2b557c3fd41db2aeb9d86723cd6edcf425a44
Abraham-kuol <abraham.kuol@strathmore.edu>
2026-04-05 15:47:01 +0300

    Use MEDIA_ROOT env for uploaded media

--- koul_health_solutions/settings.py
+++ koul_health_solutions/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# Allow a persistent media mount in production (e.g. Render disk).
+MEDIA_ROOT = Path(os.environ.get("MEDIA_ROOT", str(BASE_DIR / "media")))
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
